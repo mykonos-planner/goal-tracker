9b9171ff16f23b048f75394e9d9dc1fe7ead4332
Elias Meliani <meliani.meliani0606@gmail.com>
2026-09-01 19:10:11 +0200

    Aggiunti colori ruoli e multi ruoli 5

--- src/data/lineups.js
+++ src/data/lineups.js
@@ -1,83 +1,83 @@
 const lineups = {
   'Atalanta': {
-    starters: ['carnesecchi', 'zappacosta', 'scalvini', 'kristensen-dc', 'bernasconi', 'ederson', 'gaetano', 'de-ketelaere', 'raspadori'],
-    possible: ['samardzic', 'kristensen-dc'],
+    starters: ['carnesecchi', 'scalvini', 'kristensen', 'ederson', 'gaetano', 'de-ketelaere'],
+    possible: ['bellanova', 'zappacosta', 'bernasconi', 'kolasinac', 'samardzic', 'pasalic', 'scamacca', 'krstovic', 'raspadori', 'zalewski'],
   },
   'Bologna': {
-    starters: ['skorupski', 'zortea', 'miranda-ds', 'bernardeschi', 'orsolini-ala', 'cambiaghi-ala'],
-    possible: ['cambiaghi-ala'],
+    starters: ['skorupski', 'zortea', 'helland', 'heggem', 'miranda-ds', 'ferguson', 'moro-n', 'orsolini-ala', 'rowe'],
+    possible: ['bernardeschi', 'odgaard', 'dovbyk', 'piccoli'],
   },
   'Cagliari': {
-    starters: ['caprile', 'mina', 'obert', 'maldini', 'felici'],
-    possible: ['felici'],
+    starters: ['caprile', 'ze-pedro', 'mina', 'rodriguez-ju', 'obert', 'adopo', 'winks', 'romano', 'maldini', 'fazzini'],
+    possible: ['kevin-carlos', 'mendy-p'],
   },
   'Como': {
-    starters: ['butez', 'chalobah-dc', 'ramon', 'da-cunha', 'perrone', 'diao', 'paz-n', 'rodriguez-je'],
-    possible: [],
+    starters: ['butez', 'couto', 'ramon', 'chalobah-dc', 'kaiki', 'da-cunha', 'perrone', 'paz-n', 'baturina', 'douvikas'],
+    possible: ['diao', 'rodriguez-je'],
   },
   'Fiorentina': {
-    starters: ['de-gea', 'jimenez-ds', 'dragusin', 'viery-dc', 'valdepenas', 'fagioli', 'atta', 'mastantuono', 'oulai'],
-    possible: ['oulai'],
+    starters: ['de-gea', 'dragusin', 'viery-dc', 'valdepenas', 'atta', 'mastantuono', 'kean', 'gudmundsson'],
+    possible: ['jimenez-a', 'dodo', 'ndour', 'mandragora', 'oulai', 'fagioli'],
   },
   'Frosinone': {
-    starters: ['palmisani', 'oyono', 'bracaglia-dc', 'calo', 'kvernadze-ala', 'ghedjemis-ala'],
-    possible: ['zerbin'],
+    starters: ['oyono-a', 'calo', 'zerbin', 'hasa', 'raimondo'],
+    possible: ['palmisani', 'desplanches', 'monterisi', 'akpoguma', 'calvani', 'cittadini', 'terzic', 'bracaglia-dc', 'grillitsch', 'schmid', 'kvernadze-ala'],
   },
   'Genoa': {
-    starters: ['bijlow', 'marcandalli', 'ostigard', 'vasquez-dc', 'mitaj', 'vitinha'],
-    possible: [],
+    starters: ['bijlow', 'marcandalli', 'ostigard', 'vasquez-dc', 'norton-cuffy', 'frendrup', 'baldanzi', 'vitinha', 'colombo-g'],
+    possible: ['sow', 'amorim', 'ellertsson', 'mitaj', 'traore-hj'],
   },
   'Inter': {
-    starters: ['martinez-jo', 'bisseck', 'bastoni', 'barella', 'calhanoglu', 'dimarco', 'zielinski', 'mkhitaryan'],
-    possible: ['zielinski'],
+    starters: ['martinez-jo', 'akanji', 'bastoni', 'spence', 'barella', 'calhanoglu', 'dimarco', 'martinez-l', 'thuram'],
+    possible: ['stones', 'bisseck', 'jones-c', 'zielinski', 'sucic', 'esposito-fp'],
   },
   'Juventus': {
-    starters: ['kalulu-dc', 'bremer', 'cambiaso-ds', 'locatelli', 'mckennie', 'conceicao-ala', 'yildiz', 'alajbegovic', 'boga-ala'],
-    possible: ['celik', 'boga-ala', 'alajbegovic'],
+    starters: ['vicario', 'bremer', 'cambiaso', 'conceicao-ala', 'alajbegovic', 'yildiz', 'kolo-muani'],
+    possible: ['kalulu', 'celik', 'kelly-l', 'lucumi', 'locatelli', 'douglas-luiz', 'mckennie', 'thuram-k'],
   },
   'Lazio': {
-    starters: ['mandas', 'marusic-ds', 'doekhi', 'isaksen', 'zaccagni-ala', 'cataldi', 'cancellieri-ala', 'pedraza'],
-    possible: ['cataldi', 'cancellieri-ala', 'pedraza'],
+    starters: ['mandas', 'marusic-ds', 'doekhi', 'pedraza', 'rovella', 'taylor-k', 'frattesi', 'zaccagni-ala'],
+    possible: ['sutalo', 'provstgaard', 'isaksen', 'cancellieri-ala', 'dia', 'ratkov'],
   },
   'Lecce': {
-    starters: ['falcone', 'tiago-gabriel', 'gallo', 'ndri'],
-    possible: ['ndri'],
+    starters: ['falcone', 'veiga-d', 'gaspar-k', 'tiago-gabriel', 'gallo', 'ngom', 'pierotti', 'gandelman', 'ndri'],
+    possible: ['coulibaly-l', 'berisha-m', 'geubbels', 'stulic'],
   },
   'Milan': {
-    starters: ['maignan', 'gila', 'gabbia', 'pavlovic', 'chukwueze', 'modric', 'rabiot', 'bartesaghi', 'pulisic', 'saelemaekers'],
-    possible: ['saelemaekers'],
+    starters: ['maignan', 'gila', 'gabbia', 'pavlovic', 'rabiot', 'modric', 'pulisic', 'ramos-g'],
+    possible: ['de-winter', 'saelemaekers', 'chukwueze', 'moreira', 'bartesaghi', 'loftus-cheek', 'leao'],
   },
   'Monza': {
-    starters: ['thiam', 'birindelli-ds', 'mangas', 'pessina'],
+    starters: ['thiam', 'birindelli-ds', 'lucchesi', 'carboni-a', 'bakoune', 'pessina', 'akinsanmiro', 'mangas', 'colpani', 'robinson-j', 'cutrone'],
     possible: [],
   },
   'Napoli': {
-    starters: ['meret', 'di-lorenzo', 'rrahmani', 'spinazzola', 'mctominay', 'politano', 'neres-ala', 'santos-ala'],
-    possible: ['neres-ala'],
+    starters: ['meret', 'di-lorenzo', 'rrahmani', 'lobotka', 'mctominay', 'hojlund', 'santos-ala'],
+    possible: ['badiashile', 'beukema', 'spinazzola', 'olivera', 'zambo-anguissa', 'de-bruyne', 'politano', 'neres-ala'],
   },
   'Parma': {
-    starters: ['delprato', 'valeri', 'bernabe', 'ondrejka-ala'],
-    possible: ['ondrejka-ala'],
-  },
-  'Roma': {
-    starters: ['svilar', 'mancini', 'ndicka', 'cristante', 'kone', 'dybala', 'soule', 'koulierakis'],
-    possible: ['koulierakis', 'soule'],
-  },
-  'Sassuolo': {
-    starters: ['muric', 'idzes', 'thorstvedt'],
+    starters: ['daffara', 'delprato', 'ndiaye', 'troilo', 'valeri', 'bernabe', 'nicolussi-caviglia', 'keita-m', 'toure-e', 'romero-d', 'frigan'],
     possible: [],
   },
+  'Roma': {
+    starters: ['svilar', 'mancini', 'ndicka', 'molina', 'kone-m', 'wesley', 'dybala', 'malen'],
+    possible: ['koulierakis', 'hermoso', 'cristante', 'el-aynaoui', 'mora', 'soule'],
+  },
+  'Sassuolo': {
+    starters: ['muric', 'walukiewicz', 'cande', 'idzes', 'matic', 'adzic', 'berardi', 'lauriente'],
+    possible: ['obrador', 'doig', 'thorstvedt', 'lipani', 'pinamonti', 'bowie'],
+  },
   'Torino': {
-    starters: ['coco', 'ismajli', 'fitz-jim', 'cacciamani', 'casadei', 'adams'],
-    possible: ['ismajli', 'adams'],
+    starters: ['paleari', 'comuzzo', 'comert', 'pedersen', 'gineitis', 'cacciamani', 'vlasic', 'simeone'],
+    possible: ['coco', 'ismajli', 'fitz-jim', 'ilkhan', 'oristanio', 'casadei'],
   },
   'Udinese': {
-    starters: ['okoye', 'kabesele', 'solet', 'vojvoda', 'ekkelenkamp', 'zaniolo', 'kristensen-dc'],
-    possible: ['kristensen-dc'],
+    starters: ['okoye', 'kabasele', 'solet', 'vojvoda', 'ekkelenkamp', 'karlstrom', 'kamara-h', 'zaniolo', 'davis-k'],
+    possible: ['bertola', 'palma', 'piotrowski', 'unai-gomez'],
   },
   'Venezia': {
-    starters: ['stankovic-f', 'hainaut', 'basic', 'busio', 'yeboah', 'haps'],
-    possible: ['haps'],
+    starters: ['stankovic-f', 'bella-kotchap', 'schingtienne', 'correia-t', 'busio', 'basic', 'haps', 'yeboah'],
+    possible: ['moreno-m', 'halhal', 'sohm', 'perez-k', 'adams-a', 'rrahmani-al'],
   },
 };
 
